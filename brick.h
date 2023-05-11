7e7e351b02cedeedec836205752b245bca98c850
Oleksandr Shekera <oleksandrshekera@gmail.com>
2023-05-11 16:53:46 +0300

    Added brick model and logic

---
+++ brick.h
@@ -0,0 +1,25 @@
+#ifndef BRICK_H
+#define BRICK_H
+#include <QImage>
+#include <QRect>
+
+class Brick {
+
+public:
+    Brick(int, int);
+    ~Brick();
+
+public:
+    bool isDestroyed();
+    void setDestroyed(bool);
+    QRect getRect();
+    void setRect(QRect);
+    QImage & getImage();
+
+private:
+    QImage image;
+    QRect rect;
+    bool destroyed;
+};
+
+#endif // BRICK_H
